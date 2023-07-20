cc950b74d4124d70a985ed9f9a7f476b8a3cc821
Yana Dziadkouskaya <yana.dziadkouskaya@gmail.com>
2023-07-20 15:51:58 +0200

    Initial Commit

---
+++ src/test/java/com/dziadkouskaya/FootballScoreboard/FootballScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dziadkouskaya.FootballScoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
